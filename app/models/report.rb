dd7e62091385e8099cb1e0c91ae292a0e8ff6182
SupremeA <supreme036@gmail.com>
2016-02-05 21:12:53 -0600

    added highcharts

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,7 +1,12 @@
 class Report < ActiveRecord::Base
 
-  validates :contract, :price, presence: true :message => "CSV must have a contract and price."
+  validates :contract, :price, presence: true
 
+
+  scope :corn, -> { where(contract: "Corn") } #sorts data set by contract name
+  scope :organic, -> { where(contract: "Organic Corn") }
+
+ #imports csv file into model first row is header and must be in contract, start_time, end_time, source, price format
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
       Report.create row.to_hash
